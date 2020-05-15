45127d1d1b0c4f9bfe6eea5ba5fd1f52e965af8a
Mali McCalla <malimccalla@gmail.com>
2020-05-15 16:39:53 +0100

    Test github action for create release

--- worker/index.js
+++ worker/index.js
@@ -8,6 +8,8 @@ const redisClient = redis.createClient({
 });
 const sub = redisClient.duplicate();
 
+console.log('change');
+
 function fib(index) {
   if (index < 2) return 1;
   return fib(index - 1) + fib(index - 2);
